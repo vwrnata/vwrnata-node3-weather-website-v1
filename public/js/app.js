8f2bc664076bdf9b0c9ba32110e1cd50dc98d2a6
Vojtech Wrnata <vojtech.wrnata@red-fox.cz>
2020-02-07 10:54:12 +0100

    změna jména na mě

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,3 @@
-// console.log('Client side javascript file is loaded!')
-
-//  fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//      response.json().then((data) => {
-//         console.log(data)
-//      })
-//  })
-
-// fetch('http://localhost:3000/weather?address=liberec').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#message-1')
--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Vojtech Wrnata'
     })
 })
 
